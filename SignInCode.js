1e772ef6b6613271949fb10e2936f55bb3bd6c0f
Divyang Verma <divyang.verma26@gmail.com>
2024-10-22 15:26:18 -0400

    Create SignInCode.js

---
+++ SignInCode.js
@@ -0,0 +1,13 @@
+
+//are you in the system?
+function validate(){
+var UserName=document.getElementById("user").value;
+var Pass=document.getElementById("password").value;
+var SQLcheckEdvenualy="bet"
+var SQLcheckEdvenualy2="bet2"
+if( UserName === SQLcheckEdvenualy){// Replace Divyang Verma/Ben Hall/ Hacker with MySQL or access to php/sql
+  if(Pass === SQLcheckEdvenualy2){
+	alert("LOG IN");// link to home page(html)
+  }
+}
+
