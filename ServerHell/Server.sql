f0ef09a6210cd4c4f34578e71e09bacfcb61278c
Green13248 <149907310+Green13248@users.noreply.github.com>
2024-10-23 18:24:57 -0400

    Update Server.sql
    The Security key is like your dog's name, favorite color, childhood house, something only you would know btw

--- ServerHell/Server.sql
+++ ServerHell/Server.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE DoThatHomeWorkStudents;
+
+CREATE TABLE Persons (
+    PersonID int,
+    Username varchar(255),
+    Password varchar(255),
+    Email varchar(255)
+    Securitykey varchar(255)
+);
