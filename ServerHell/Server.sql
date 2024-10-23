eb2b7614a51936386553609b16be2cbbaf6e61b6
Green13248 <149907310+Green13248@users.noreply.github.com>
2024-10-23 18:16:30 -0400

    Update Server.sql

--- ServerHell/Server.sql
+++ ServerHell/Server.sql
@@ -1,15 +1,1 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<?php
-$txt = "PHP";
-echo "I hate $txt! This is a PLACEHOLDER!";
-?>
-
-//This is similar to c++!
-  
-</body>
-</html>
-
-
+CREATE DATABASE DoThatHomeWorkStudents;
